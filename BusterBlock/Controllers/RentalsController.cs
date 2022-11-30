e5cfcd45b215fd23ac22d1cd7c3cbf7fc8ada2e4
Michael <mike.tompk@gmail.com>
2022-11-29 19:45:24 -0500

    temporary Rental return before rework

--- BusterBlock/Controllers/RentalsController.cs
+++ BusterBlock/Controllers/RentalsController.cs
@@ -1,10 +1,8 @@
-﻿using BusterBlock.Models;
-using BusterBlock.ViewModels;
-using System.Collections;
-using System.Collections.Generic;
-using System.Web.Mvc;
-using System.Data.Entity;
+﻿using BusterBlock.ViewModels;
 using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace BusterBlock.Controllers
 {
@@ -20,14 +18,31 @@ namespace BusterBlock.Controllers
 
         public ActionResult Rentals()
         {
-            RentalListViewModel rentalListViewModel = new RentalListViewModel(_context.Rentals.Include(r => r.Movie).Include(r => r.Customer));
+            RentalListViewModel rentalListViewModel = new RentalListViewModel
+            (
+                _context.Rentals.Include(r => r.Movie).Include(r => r.Customer).Where(r => r.DateReturned == null)
+            );
 
             return View(rentalListViewModel);
         }
 
-        public ActionResult Return()
+        public ActionResult Return(int id)
         {
-            throw new NotImplementedException();
+            var rental = _context.Rentals.SingleOrDefault(r => r.Id == id);
+
+            if (rental == null)
+            {
+                return HttpNotFound();
+            }
+
+            rental.DateReturned = DateTime.Now;
+
+            _context.SaveChanges();
+
+            return View("Rentals", new RentalListViewModel
+            (
+                _context.Rentals.Include(r => r.Movie).Include(r => r.Customer).Where(r => r.DateReturned == null)
+            ));
         }
 
         #endregion
